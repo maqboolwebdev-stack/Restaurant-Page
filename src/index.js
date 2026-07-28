dcb3bc3e8f5bf971e6c96d8d8cf5620b2b87b771
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-07-28 15:56:37 +0500

    Compete the project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./styles/styles.css";
 import "./styles/css-resets.css";
-import { greeting } from "./greeting.js";
+import "./home/home.js"
+import "./home/home.css"
+import "./menu/menu.js"
+import "./menu/menu.css"
+import "./about/about.js"
+import "./about/about.css"
 
-console.log(greeting);
+console.log('Live Present MOMENT');
